37a23345bdfa41da2341e93ead47d4359bfa2c2c
beshoynasryz <beshoynasry1@gmail.com>
2025-01-21 07:50:54 +0200

    user model , create User 🚀

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -4,10 +4,54 @@ import { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema()
+@Schema({timestamps: true} )
 export class User {
-  @Prop()
+  @Prop({
+    type : String, 
+    required :true,
+    min : [3, 'Name is too short'],
+    max : [20, 'Name is too long']
+  })
   name: string;
+//   @Prop({
+//     required :true,
+//     unique : true
+//   })
+//   email :string
+
+//   @Prop({
+//     type : String,
+//     required : true,
+//     min : [6, 'Password is too short'],
+//     max : [200, 'Password is too long']
+//   })
+//   password :string
+
+//   @Prop({
+//     type : String,
+//     required : true,
+//     enum : ['user', 'admin'],
+//     default : 'user'
+//   })
+//   role :string
+//   @Prop({
+//     type :String,
+//     enum : ['male', 'female'],
+//   })
+//   gender :string
+//   @Prop({
+//     type :Number,
+//     required : true,
+    
+//   })
+//   age :number
+
+//   @Prop({
+//     type :String ,
+//     required : true,
+
+//   })
+//   phoneNumber :string
 
 }
 
